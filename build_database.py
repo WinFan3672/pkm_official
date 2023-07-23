cdec956c4865de5a1e789a56c04aa92d6b3fd8d8
WinFan3672 <winfan3672@gmail.com>
2023-07-23 22:34:35 +0100

    Migrated packages from 000webhost

--- build_database.py
+++ build_database.py
@@ -6,12 +6,12 @@ print("build: Beginning build process.")
 for item in os.listdir():
     if os.path.isdir(item):
         ## Check for contents
-        if "package.szip3" in os.listdir(item) and "package.info" in os.listdir(item):
+        if "package.szip3" in os.listdir(item) and "program.info" in os.listdir(item):
             url = "https://github.com/WinFan3672/pkm_official/raw/main/{}/package.szip3".format(item)
-            with open(f"{item}/package.info") as f:
+            with open(f"{item}/program.info") as f:
                 g = f.read().split("|")
                 if len(g) != 2:
-                    print("Invalid package info for {}.".format(item))
+                    print("ERROR: Invalid package info for {}.".format(item))
                     continue
             dic = {"name":g[0],
                 "url":url,
@@ -20,6 +20,6 @@ for item in os.listdir():
             db[item] = dic
         else:
             print("ERROR: Package '{}' has missing files.".format(item))
-with open("community.db.cfg","wb") as f:
+with open("pkm.db.cfg","wb") as f:
     pickle.dump(db,f)
 print("Build finished.")

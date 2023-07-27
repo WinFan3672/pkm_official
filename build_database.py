4cbb2ea5da46aa7dd25ddd1efe11a0f84b8f9bf5
WinFan3672 <winfan3672@gmail.com>
2023-07-27 17:46:21 +0100

    Updated build_database.py

--- build_database.py
+++ build_database.py
@@ -1,13 +1,21 @@
 #!/usr/bin/python
 import os
 import pickle
+import zipfile
 db = {}
-print("build: Beginning build process.")
+EXCLUSION_LIST = [".git"]
+URL_BASE = "https://github.com/WinFan3672/pkm_official/raw/main/"
+print("Beginning build process.")
 for item in os.listdir():
-    if os.path.isdir(item):
-        ## Check for contents
+    if os.path.isdir(item) and item not in EXCLUSION_LIST:
         if "package.szip3" in os.listdir(item) and "program.info" in os.listdir(item):
-            url = "https://github.com/WinFan3672/pkm_official/raw/main/{}/package.szip3".format(item)
+            with open("{}/package.szip3".format(item),"rb") as f:
+                with zipfile.ZipFile(f,"r") as zf:
+                    with zf.open("program.info","r") as zff:
+                        x = zff.readlines()[0].decode("utf-8")
+                        x = x.split("|")
+                        fileVersion = x[1].split(".")
+            url = "{}{}/package.szip3".format(URL_BASE,item)
             with open(f"{item}/program.info") as f:
                 g = f.read().split("|")
                 if len(g) != 2:
@@ -16,6 +24,7 @@ for item in os.listdir():
             dic = {"name":g[0],
                 "url":url,
                 "desc":g[1],
+                "version":fileVersion,
                 }
             db[item] = dic
         else:
